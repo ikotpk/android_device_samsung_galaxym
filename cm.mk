15f29568903691ee0595477eea38eab2b277ecf5
ikotpk <ik__ot__pk@naver.com>
2013-04-10 18:07:33 +0900

    Commit 03
    Modify the source to fit the Galaxy M Style Galaxy S based devices.

--- cm.mk
+++ cm.mk
@@ -2,19 +2,19 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := GalaxyS
+PRODUCT_RELEASE_NAME := GalaxyM
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/galaxysmtd/full_galaxysmtd.mk)
+$(call inherit-product, device/samsung/galaxym/full_galaxym.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := galaxysmtd
-PRODUCT_NAME := cm_galaxysmtd
+PRODUCT_DEVICE := galaxym
+PRODUCT_NAME := cm_galaxym
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SHW-M110S
+PRODUCT_MODEL := SHW-M340S
 
 #Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SHW-M110S TARGET_DEVICE=SHW-M110S BUILD_FINGERPRINT=samsung/SHW-M110S/SHW-M110S:2.3.6/GINGERBREAD/WF07:user/release-keys PRIVATE_BUILD_DESC="SHW-M110S-user 2.3.6 GINGERBREAD WF07 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SHW-M340S TARGET_DEVICE=SHW-M340S BUILD_FINGERPRINT=samsung/SHW-M340S/SHW-M110S:2.3.6/GINGERBREAD/WF07:user/release-keys PRIVATE_BUILD_DESC="SHW-M110S-user 2.3.6 GINGERBREAD WF07 release-keys"
